2235defdce0abc6737b9dba0483308e91d4bfa9b
Gary Firzon <gfirzon@yahoo.com>
2019-03-07 19:03:48 -0600

    real swagger project

--- ProductsApi/Startup.cs
+++ ProductsApi/Startup.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace ProductsApi
 {
@@ -25,11 +29,49 @@ namespace ProductsApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddOptions();
+
+            services.AddMvcCore().AddVersionedApiExplorer(o => o.GroupNameFormat = "'v'VVV");
+            //services.AddMvcCore();
+            //services.AddMvc();
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // allow a client to call you without specifying an api version
+            // since we haven't configured it otherwise, the assumed api version will be 1.0
+            services.AddApiVersioning(o => o.AssumeDefaultVersionWhenUnspecified = true);
+
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(
+                    options =>
+                    {
+                        var provider = services.BuildServiceProvider()
+                                               .GetRequiredService<IApiVersionDescriptionProvider>();
+
+
+                        foreach (var description in provider.ApiVersionDescriptions)
+                        {
+                            options.SwaggerDoc(description.GroupName, CreateInfoForApiVersion(description));
+                        }
+
+                        // add a custom operation filter which sets default values
+                        //options.OperationFilter<SwaggerDefaultValues>();
+
+                        options.IncludeXmlComments(XmlCommentsFilePath);
+                    });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", b => b.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials()
+                );
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApiVersionDescriptionProvider provider)
         {
             if (env.IsDevelopment())
             {
@@ -40,8 +82,57 @@ namespace ProductsApi
                 app.UseHsts();
             }
 
+            app.UseStaticFiles();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+            app.UseCors("CorsPolicy");
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(
+                options =>
+                {
+                    foreach (var description in provider.ApiVersionDescriptions)
+                    {
+                        options.SwaggerEndpoint(
+                            $"/swagger/{description.GroupName}/swagger.json",
+                            description.GroupName.ToUpperInvariant());
+                    }
+                });
+
             app.UseHttpsRedirection();
             app.UseMvc();
+        }
+
+        static string XmlCommentsFilePath
+        {
+            get
+            {
+                var basePath = System.AppContext.BaseDirectory;
+                //var basePath = PlatformServices.Default.Application.ApplicationBasePath;
+                var fileName = typeof(Startup).GetTypeInfo().Assembly.GetName().Name + ".xml";
+                return Path.Combine(basePath, fileName);
+            }
+        }
+
+        static Info CreateInfoForApiVersion(ApiVersionDescription description)
+        {
+            var info = new Info()
+            {
+                Title = $"Products API {description.ApiVersion}",
+                Version = description.ApiVersion.ToString(),
+                Description = "A Products API application with Swagger, Swashbuckle, and API versioning.",
+                Contact = new Contact() { Name = "Gary Firzon", Email = "gfirzon@yahoo.com" },
+                TermsOfService = "Proprietory",
+                License = new License() { Name = "MIT", Url = "https://opensource.org/licenses/MIT" }
+            };
+
+            if (description.IsDeprecated)
+            {
+                info.Description += " This API version has been deprecated.";
+            }
+
+            return info;
         }
     }
 }
