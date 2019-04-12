0d54423b8fcf66ef47d3cf1b7e5c10418dbe393c
snikitin2018 <snikitin1981@gmail.com>
2019-04-12 14:32:49 -0500

    Added products parts

---
+++ ProductsApi/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsApi.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string ProductDescription { get; set; }
+        public int UnitsInStock { get; set; }
+        public decimal SellPrice { get; set; }
+        public int DiscountPercentage { get; set; }
+        public int UnitsMax { get; set; }
+    }
+}
---
+++ ProductsApi/Repositories/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using ProductsApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductsApi.Repositories
+{
+    public interface IProductRepository
+    {
+        List<Product> GetProductList();
+        Product GetProduct(int id);
+        int CreateProduct(Product product);
+        void UpdateProduct(Product product);
+    }
+}
