07ad0a020f6211c8bf8fcb8e183c72e80e867f82
snikitin2018 <snikitin1981@gmail.com>
2019-05-02 14:00:09 -0500

    Added user service tests

--- ProductTests/ServiceTests/UserServiceTests.cs
+++ ProductTests/ServiceTests/UserServiceTests.cs
@@ -1,4 +1,6 @@
 ﻿using Moq;
+using System.Linq;
+using System.Threading.Tasks;
 using ProductsApi.Models;
 using ProductsApi.Repositories;
 using ProductsApi.Services;
@@ -58,6 +60,119 @@ namespace ProductTests.ServiceTests
 
             // Asserts
             Assert.Null(user);
+        }
+        [Fact]
+        public void CreateUser_Returns_New_Id_When_Created()
+        {
+            // Arrange
+            int id = int.MaxValue;
+            var user = new User
+            {
+                UserID = 10,
+                RoleID = 1,
+                IsActive = true,
+                UserName = "lala@lala.com",
+                UserPassword = "lalala"
+            };
+
+            mockRepository.Setup(m => m.CreateUser(user)).Returns(id);
+
+            // Act
+            id = service.CreateUser(user);
+
+            // Asserts
+            Assert.NotNull(user);
+            Assert.Equal(10, user.UserID);
+        }
+        [Fact]
+        public void GetAll_Returns_UserList_When_All_Valid()
+        {
+            // Arrange
+            List<User> list = new List<User>();
+            mockRepository.Setup(m => m.GetUserList()).Returns(list);
+
+            // Act
+            list = service.GetUserList();
+
+            // Asserts
+            Assert.NotNull(list);
+        }
+
+        [Fact]
+        public void UpdateUser_Returns_true_When_dbUser_Not_Null()
+        {
+            // Arrange
+            bool result = false;
+          
+            var user = new User
+            {
+                UserID = 12,
+                RoleID = 3,
+                IsActive = true,
+                UserName = "pa@gmail.com",
+                UserPassword = "Polya"
+            };
+
+            User dbUser = new User();
+            dbUser = service.GetUser(dbUser.UserID);
+            mockRepository.Setup(m => m.UpdateUser(dbUser));
+
+            // Act
+            result = service.UpdateUser(user);
+
+            // Asserts
+            Assert.False(result);
+        }
+        [Fact]
+        public void GetAll_Returns_ViewModelList_When_All_Valid()
+        {
+            // Arrange
+            List<UserViewModel> list = new List<UserViewModel>();
+            mockRepository.Setup(m => m.GetUserViewModelList()).Returns(list);
+
+            // Act
+            list = service.GetUserViewModelList();
+
+            // Asserts
+            Assert.NotNull(list);
+        }
+        [Fact]
+        public void GetUserViewModelList_Returns_UserViewModel_When_Found()
+        {
+            // Arrange
+            int id = int.MaxValue;
+
+            mockRepository.Setup(m => m.GetUserViewModel(
+                It.IsAny<int>()
+                )).Returns(new UserViewModel
+                {
+                    UserName = "AAA"
+                });
+
+            // Act
+            UserViewModel userView = service.GetUserViewModel(id);
+
+            // Asserts
+            Assert.NotNull(userView);
+            Assert.NotNull(userView.UserName);
+        }
+        [Fact]
+        public void GetUserViewModel_Returns_Null_When_NotFound()
+        {
+            // Arrange
+            int id = int.MaxValue;
+
+            UserViewModel repoUser = null;
+
+            mockRepository.Setup(m => m.GetUserViewModel(
+                It.IsAny<int>()
+                )).Returns(repoUser);
+
+            // Act
+            UserViewModel userView = service.GetUserViewModel(id);
+
+            // Asserts
+            Assert.Null(userView);
         }
     }
 }
